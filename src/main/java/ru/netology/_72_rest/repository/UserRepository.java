462235b9d7f5f8fb4d07472f98960e3a71495a52
Vitaly Shvedko <davids013013@gmail.com>
2022-02-23 17:55:49 +0300

    Add test property for repository

--- src/main/java/ru/netology/_72_rest/repository/UserRepository.java
+++ src/main/java/ru/netology/_72_rest/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package ru.netology._72_rest.repository;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 import ru.netology._72_rest.domain.Authorities;
 import ru.netology._72_rest.domain.User;
@@ -12,10 +13,15 @@ import java.util.concurrent.ConcurrentHashMap;
 public class UserRepository {
     private final Map<String, User> users = new ConcurrentHashMap<>();
 
+    public UserRepository(@Value("#{new Boolean('${custom.is-test:false}')}") boolean isTest) {
+//        Добавление тестового пользователя
+        if (isTest) {
+            users.put("test", new User("test", "test", List.of(Authorities.READ)));
+        }
+        System.out.println(this + " run in test mode: " + isTest);
+    }
+
     public List<Authorities> getUserAuthorities(User user) {
-////        Добавление тестового пользователя
-//        users.put("testUser1", new User(
-//                "testUser1", "iddqd", List.of(Authorities.READ, Authorities.WRITE)));
         final String name = user.getName();
         if (users.containsKey(name) && users.get(name).getPassword().equals(user.getPassword())) {
             return users.get(name).getAuthorities();
